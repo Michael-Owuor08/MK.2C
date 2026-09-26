47ae50cc27065f5e3bca3ae9abcf595a1e2b77c4
Michael Owuor <michael.otister@gmail.com>
2026-09-26 10:14:35 +0300

    Fix description and format bank balance output

---
+++ Height_bankBalance_phoneNumber1.c.c
@@ -0,0 +1,32 @@
+/*
+Author:Michael 
+Reg number:BCS-01-0001/2026
+Description:Program to prompt user to enter personal details 
+Date:21/09/2026
+
+*/
+
+#include <stdio.h>
+
+int main(){
+
+    float height; 
+    char meter='m';
+    int amount; 
+    char number[11];
+
+    printf("My height is:\t"); 
+    scanf ("%f",&height);
+
+    printf("My bank account balance is:\t"); 
+    scanf("%d",&amount);
+
+    printf("My phone number is:\t");
+    scanf("%s",number);
+
+    printf ("\nMy height is:%.2f%c\n",height, meter);  
+    printf("My bank account balance is:Ksh%.2d%s\n", amount, Ksh); 
+    printf("My phone number is:%s\n", number);
+
+    return 0; 
+}
